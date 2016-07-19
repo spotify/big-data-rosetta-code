1053474989c86615bd42f22ef0001e28f0be0640
Neville Li <neville.lyh@gmail.com>
2016-07-18 21:16:29 -0400

    test examples (#7)
    * add scio-test dependency
    
    * add scalacheck dependency
    
    * add test examples

--- build.sbt
+++ build.sbt
@@ -13,8 +13,10 @@ javacOptions ++= Seq("-source", "1.7", "-target", "1.7")
 libraryDependencies ++= Seq(
   "com.spotify" %% "scio-core" % "0.1.9",
   "com.spotify" %% "scio-extra" % "0.1.9",
+  "com.spotify" %% "scio-test" % "0.1.9" % "test",
   "com.twitter" %% "scalding-core" % "0.16.0",
   "com.twitter" %% "algebird-spark" % "0.12.1",
   "org.apache.spark" %% "spark-core" % "1.6.2",
-  "org.apache.spark" %% "spark-mllib" % "1.6.2"
+  "org.apache.spark" %% "spark-mllib" % "1.6.2",
+  "org.scalacheck" %% "scalacheck" % "1.13.2" % "test"
 )
---
+++ src/test/scala/com/spotify/bdrc/testing/EndToEndTest.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2016 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.bdrc.testing
+
+import com.spotify.scio._
+import com.spotify.scio.testing._
+
+object WordCount1 {
+  def main(cmdlineArgs: Array[String]): Unit = {
+    val (sc, args) = ContextAndArgs(cmdlineArgs)
+    sc.textFile(args("input"))
+      .flatMap(_.split("[^a-zA-Z']+").filter(_.nonEmpty))
+      .countByValue
+      .map(kv => kv._1 + ": " + kv._2)
+      .saveAsTextFile(args("output"))
+  }
+}
+
+/**
+ * Test an entire pipeline end-to-end
+ *
+ * Pros:
+ * - Complete test of the entire pipeline
+ * - Covers argument parsing and I/O handling
+ * - May also reveal serialization issues
+ *
+ * Cons:
+ * - Hard to handcraft input and expected data
+ * - Hard to cover edge cases for complex pipelines
+ * - Can be slow in some frameworks
+ *
+ * Supported in: Scalding, Scio
+ *
+ * Recommendation:
+ * This is a good approach to test small and simple pipelines since it offers the best code
+ * coverage. It can also be used for pipelines with complex argument parsing and I/O handling,
+ * e.g. ones with dynamic I/O based on arguments.
+ *
+ * Very complex pipelines with lots of steps may be broken down into smaller logical blocks and
+ * tested separately using the transform test approach.
+ */
+class EndToEndTest extends PipelineSpec {
+
+  val input = Seq("a b c d e", "a b a b")
+  val expected = Seq("a: 3", "b: 3", "c: 1", "d: 1", "e: 1")
+
+  "WordCount1" should "work" in {
+    JobTest[com.spotify.bdrc.testing.WordCount1.type]
+      .args("--input=in.txt", "--output=out.txt")
+      .input(TextIO("in.txt"), input)
+      .output(TextIO("out.txt")) { output =>
+        output should containInAnyOrder (expected)
+      }
+      .run()
+  }
+
+}
